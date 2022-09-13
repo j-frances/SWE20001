6422f652e21428c4c17c12867bf518f7de0dd863
Ollygup <82217636+ollygup@users.noreply.github.com>
2022-09-13 18:55:51 +1000

    Add files via upload

---
+++ EditProduct.php
@@ -0,0 +1,34 @@
+<?php
+
+$mysqli = include('connect.php');
+
+// Check connection
+if ($mysqli -> connect_errno) {
+    echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
+    exit();
+    header("Location: index.php");
+}
+  
+// input value
+$ID = $_POST['ID'];
+$productName = $_POST['ProductName'];
+$quantity = $_POST['Quantity'];
+$price = $_POST['Price'];
+
+$select = mysqli_query($mysqli, "SELECT `ID` FROM `Products` WHERE `ID` = '".$_POST['ID']."'") or exit(mysqli_error($mysqli));
+if(mysqli_num_rows($select)) {
+    $sql = "UPDATE `Products` SET `ProductName` = \"$productName\", `Quantity`= \"$quantity\", `Price` = \"$price\" WHERE `ID` = \"$ID\"";
+    mysqli_query($link, $sql);
+    header("Location: https://gotogro.000webhostapp.com/Product.php");
+}else{
+    echo "Product does not exist";
+}
+// Attempt insert query execution
+//$sql = "INSERT INTO Products (ProductName, Quantity, Price) VALUES (\"$productName\",\"$quantity\",\"$price\")";
+
+
+
+ 
+// Close connection
+mysqli_close($link);
+?>
