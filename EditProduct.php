9b9e02d81b282495ad3578d3eec4832516cf8b2f
Ollygup <82217636+ollygup@users.noreply.github.com>
2022-09-20 19:16:32 +1000

    Update EditProduct.php

--- EditProduct.php
+++ EditProduct.php
@@ -10,25 +10,29 @@ if ($mysqli -> connect_errno) {
 }
   
 // input value
-$ID = $_POST['ID'];
-$productName = $_POST['ProductName'];
-$quantity = $_POST['Quantity'];
-$price = $_POST['Price'];
-
-$select = mysqli_query($mysqli, "SELECT `ID` FROM `Products` WHERE `ID` = '".$_POST['ID']."'") or exit(mysqli_error($mysqli));
-if(mysqli_num_rows($select)) {
-    $sql = "UPDATE `Products` SET `ProductName` = \"$productName\", `Quantity`= \"$quantity\", `Price` = \"$price\" WHERE `ID` = \"$ID\"";
-    mysqli_query($link, $sql);
-    header("Location: https://gotogro.000webhostapp.com/Product.php");
-}else{
-    echo "Product does not exist";
-}
+foreach($_POST['ID'] as $ID){
+    $ID = $_POST['ID'];
+    $productName = $_POST['ProductName'];
+    $quantity = $_POST['Quantity'];
+    $price = $_POST['Price'];
+    
+    
+    
+    $select = mysqli_query($mysqli, "SELECT `ID` FROM `Products` WHERE `ID` = '".$_POST['ID']."'") or exit(mysqli_error($mysqli));
+    if(mysqli_num_rows($select)) {
+        $sql = "UPDATE `Products` SET `ProductName` = \"$productName\", `Quantity`= \"$quantity\", `Price` = \"$price\" WHERE `ID` = \"$ID\"";
+        mysqli_query($link, $sql);
+        
+    }else{
+        echo "Product does not exist";
+    }
 // Attempt insert query execution
 //$sql = "INSERT INTO Products (ProductName, Quantity, Price) VALUES (\"$productName\",\"$quantity\",\"$price\")";
 
-
-
+}
+header("Location: https://gotogro.000webhostapp.com/home.php");
  
 // Close connection
 mysqli_close($link);
 ?>
+
