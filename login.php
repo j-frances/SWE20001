e5a2803b4e34c88bbec610a032c2b6a9ceb4f2fa
j-frances <jonathan.frances@live.com.au>
2022-10-05 13:27:58 +1100

    Redirect to index.php on bad login
    Previous behaviour would allow viewing of error messages on login.php which could be exploited by malicious actors. 
    Removed this so that the user is returned to index.php if the login is incorrect.

--- login.php
+++ login.php
@@ -2,13 +2,13 @@
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
 
-// These only work for CRYPT_SHA512, but it should give you an idea of how crypt() works.
-// $Salt = uniqid(); // Could use the second parameter to give it more entropy.
-// $Algo = '6'; // This is CRYPT_SHA512 as shown on http://php.net/crypt
-// $Rounds = '10000'; // The more, the more secure it is!
-//
-// // This is the "salt" string we give to crypt().
-// $CryptSalt = '$' . $Algo . '$rounds=' . $Rounds . '$' . $Salt;
+function Redirect($url)
+{
+    header('Location: ' . $url);
+
+    exit();
+}
+
 
 
 $mysqli = include('connect.php');
@@ -19,8 +19,8 @@ $password = htmlspecialchars($_POST['password']);
 // Check connection
 if ($mysqli -> connect_errno) {
   echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
-  exit();
   header("Location: index.php");
+  exit;
 }
 
 $sql = "SELECT `Salt`, `Username`, `Password` FROM `Verified_Users` WHERE `Username` = \"$username\"";
@@ -32,21 +32,24 @@ if ($result->num_rows > 0) {
     // Username exists, query successful
     $CryptSalt = $row["Salt"];
     $hashed_password = crypt($password, $CryptSalt);
+    
 
 
-    if($row["Username"] == $username && $row["Password"] == $hashed_password){
+    if($row["Username"] == $username && $row["Password"] == strval($hashed_password)){
       // Password correct, authentication successful
       session_start();
       $_SESSION["loggedin"] = "true";
       $_SESSION["username"] = $username;
-      header("Location: home.php");
+      
+      Redirect('https://gotogro.000webhostapp.com/home.php');
+      // exit;
     }else{
       // Password incorrect, authentication failed
-      header("Location: index.php");
+        header("Location: index.php");
     }
   }
 } else {
   // Username doesn't exist, query unsuccessful
-  header("Location: index.php");
+    header("Location: index.php");
 }
 ?>
