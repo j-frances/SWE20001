305d0907145ef42bd7750ebe5f5af03a6bf8ead3
j-frances <jonathan.frances@live.com.au>
2022-10-05 18:29:14 +1100

    Implement decoder of POST
    Data is now decoded from base64 when it arrives via the POST method

--- login.php
+++ login.php
@@ -2,19 +2,12 @@
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
 
-function Redirect($url)
-{
-    header('Location: ' . $url);
-
-    exit();
-}
-
-
-
 $mysqli = include('connect.php');
 
-$username = htmlspecialchars($_POST['username']);
-$password = htmlspecialchars($_POST['password']);
+
+$username = base64_decode($_POST['username']);
+$password = base64_decode($_POST['password']);
+
 
 // Check connection
 if ($mysqli -> connect_errno) {
@@ -22,6 +15,7 @@ if ($mysqli -> connect_errno) {
   header("Location: index.php");
   exit;
 }
+
 
 $sql = "SELECT `Salt`, `Username`, `Password` FROM `Verified_Users` WHERE `Username` = \"$username\"";
 
@@ -32,8 +26,6 @@ if ($result->num_rows > 0) {
     // Username exists, query successful
     $CryptSalt = $row["Salt"];
     $hashed_password = crypt($password, $CryptSalt);
-    
-
 
     if($row["Username"] == $username && $row["Password"] == strval($hashed_password)){
       // Password correct, authentication successful
@@ -41,15 +33,15 @@ if ($result->num_rows > 0) {
       $_SESSION["loggedin"] = "true";
       $_SESSION["username"] = $username;
       
-      Redirect('https://gotogro.000webhostapp.com/home.php');
-      // exit;
+      header('Location: https://gotogro.000webhostapp.com/home.php');
+      //exit;
     }else{
-      // Password incorrect, authentication failed
+        // Password incorrect, authentication failed
         header("Location: index.php");
     }
   }
 } else {
-  // Username doesn't exist, query unsuccessful
+    // Username doesn't exist, query unsuccessful
     header("Location: index.php");
 }
 ?>
